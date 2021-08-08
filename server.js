d94ff6c3fcc9acf86f99ed360c68f56c88498233
Timothy Otieno <timothyking094@gmail.com>
2021-08-08 12:36:32 +0300

    modifile

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 
-const PORT = 3001;
-
 const app = express();
 
-// set up middleware for getting body of request
-app.use(express.json);
+const PORT = 3001;
 
-// routes
-app.use('/pizzeria', require('./routes/shop'));
+// Initialise a middleware to get the body of the request
+app.use(express.json());
 
+// Routing
+app.use('/pizzeria', require('./routes/shop'))
 
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log(err.message);
-        process.exit(1)
-    }
-    console.log(`Server running on port, ${PORT}`)
-})
+app.listen(PORT, (error) => {
+    if(error) console.log(error.message);
+    console.log(`Server is running on port ${PORT}`)
+});
