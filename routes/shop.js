9620881807483b2c4ac03b1d94fb9a55d58dbd12
Timothy Otieno <timothyking094@gmail.com>
2021-08-08 12:39:03 +0300

    add routes to fetch pizza and order data from the db [starts #179127190]

--- routes/shop.js
+++ routes/shop.js
@@ -3,17 +3,44 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-router.get('/get_pizza', async (req, res) => {
+router.get('/pizza', async (req, res) => {
     try {
         const pizza = await prisma.pizza.findMany();
-        res.json(pizza);
+        res.json(pizza)
     } catch (error) {
         console.log(error.message);
         res.status(500);
         res.json({
-            "message": "Server Error, we are trying to fix it. Try again later..."
+            "message": "Server Error, we are fixing it. Try again later..."
         })
     }
 });
+
+router.get('/order/:id', async(req, res) => {
+    try {
+        console.log(req.params)
+        const order = await prisma.orderItem.findUnique({
+            where: {
+                id: req.params.id
+            },
+            include: {
+                pizza: {
+                    select: {
+                        name: true,
+                        price: true
+                    }
+                }
+            }
+        });
+        res.json(order);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500);
+        res.json({
+            "message": "Server Error, we are fixing it. Try agin later..."
+        })
+    }
+})
+
 
 module.exports = router
