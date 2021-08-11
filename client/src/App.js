0cea5bbbb93de2f5eada31bc0c6934c9c588f832
Timothy Otieno <timothyking094@gmail.com>
2021-08-11 10:13:17 +0300

    add the individual components to the main app component [finishes #179174650]

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
-
 import PizzaProvider from './context/pizzaContext/PizzaProvider';
+import { Navbar } from './components/Navbar';
+import { PizzaList } from './components/PizzaList';
 
 function App() {
   return (
     <div className="App">
       <PizzaProvider>
-        <h1> Hello World!</h1>
+        <Navbar />
+        <main className= "container">
+          <div className= "main-heading">
+            <h2> Available Pizza </h2>
+            <button>
+            <i class="fas fa-shopping-cart"></i>
+            </button>
+          </div>
+          <PizzaList />
+        </main>
       </PizzaProvider>
      
     </div>
