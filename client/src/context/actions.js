e877252f994c3e91aa6e8f94c1d90028efeed478
Timothy Otieno <timothyking094@gmail.com>
2021-08-11 14:01:17 +0300

    add app actions [starts #179174706]

--- client/src/context/actions.js
+++ client/src/context/actions.js
@@ -1,2 +1,4 @@
 export const FETCH_PIZZA = 'FETCH_PIZZA';
-export const FETCHING_ERROR = 'FETCHING_ERROR';
+export const ERROR_MESSAGE = 'ERROR_MESSAGE';
+export const ORDER_SUCCESS = 'ORDER_SUCCESS';
+export const LOAD_CART = 'LOAD_CART';
