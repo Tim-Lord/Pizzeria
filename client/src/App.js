a01c4e10b02cba3d1397054388da609248c065d6
Timothy Otieno <timothyking094@gmail.com>
2021-08-11 06:13:26 +0300

    add the PizzaProvider component [starts #179174650]
    makes global state accessible to all the components it wraps around

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 
+import PizzaProvider from './context/pizzaContext/PizzaProvider';
+
 function App() {
   return (
     <div className="App">
-     <h1> Hello World!</h1>
+      <PizzaProvider>
+        <h1> Hello World!</h1>
+      </PizzaProvider>
+     
     </div>
   );
 }
