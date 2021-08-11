691fd563f82138ab51da21d70713e760bfe38973
Timothy Otieno <timothyking094@gmail.com>
2021-08-11 09:58:21 +0300

    main page navbar [starts #179174650]

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className= 'header'>
+            <div className= 'header-container'>
+                <h1> Pizzeria </h1>
+                <p> made for all your pizza needs </p>
+            </div>
+            
+        </header>
+    )
+}
+
+export {Navbar}
